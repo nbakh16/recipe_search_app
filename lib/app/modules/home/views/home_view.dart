1352e829dd827f6171bb46ecb3c5bc8a130ff4a6
Nabil160 <nbakhunjee97@gmail.com>
2023-08-22 21:48:08 +0600

    homepage ui

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -5,19 +5,104 @@ import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.sizeOf(context).width;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
+      appBar: SearchAppBar(),
+      drawer: Drawer(),
       body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: GridView.builder(
+              physics: const ScrollPhysics(),
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: screenWidth<700 ? 2 : screenWidth<900 ? 3 : 4,
+                  childAspectRatio: 0.65
+              ),
+              itemCount: 16,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+
+                  },
+                  child: Card(
+
+                  ),
+                );
+              }
+            ),
+          ),
+        )
+      ),
+    );
+  }
+}
+
+class SearchAppBar extends StatelessWidget implements PreferredSizeWidget {
+  SearchAppBar({super.key});
+  final double appBarHeight = 86;
+
+  @override
+  Size get preferredSize => Size.fromHeight(appBarHeight);
+
+  final TextEditingController _searchTEController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      toolbarHeight: appBarHeight,
+      title: Padding(
+        padding: const EdgeInsets.only(top: 28.0, bottom: 4.0),
+        child: TextFormField(
+          controller: _searchTEController,
+          textAlignVertical: TextAlignVertical.center,
+          decoration: InputDecoration(
+            hintText: 'Search for Recipe',
+            suffixIcon: IconButton(
+              onPressed: () {
+                _searchTEController.clear();
+              },
+              icon: const Icon(Icons.cancel),
+              splashRadius: 1,
+            ),
+          ),
+          keyboardType: TextInputType.text,
+          textInputAction: TextInputAction.done,
+          validator: (String? value) {
+            if(value?.isEmpty ?? true) {
+              return 'Search field is empty';
+            }
+            return null;
+          },
         ),
       ),
+      leading: Builder(
+        builder: (context) => Padding(
+          padding: const EdgeInsets.only(top: 28.0, bottom: 4.0),
+          child: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => Scaffold.of(context).openDrawer(),
+          ),
+        ),
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(top: 28.0, bottom: 4.0),
+          child: IconButton(
+            onPressed: () {
+              _searchTEController.clear();
+            },
+            icon: const Icon(Icons.more_vert),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,75 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import 'app/data/utils/colors.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
+  runApp(const RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Recipe App",
+      debugShowCheckedModeBanner: false,
+      theme: _lightThemeData(context),
+      themeMode: ThemeMode.light,
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-    ),
-  );
+    );
+  }
+
+  ThemeData _lightThemeData(BuildContext context) {
+    return ThemeData(
+      brightness: Brightness.light,
+      primarySwatch: mainColor,
+      pageTransitionsTheme: const PageTransitionsTheme(
+        builders: <TargetPlatform, PageTransitionsBuilder>{
+          TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+          TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+        },
+      ),
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.grey.shade100,
+        elevation: 2,
+        iconTheme: const IconThemeData(
+          color: Colors.grey,
+          size: 28
+        ),
+        actionsIconTheme: const IconThemeData(
+          color: Colors.grey,
+          size: 28
+        ),
+      ),
+      inputDecorationTheme: const InputDecorationTheme(
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+            borderSide: BorderSide(
+                color: Colors.grey, width: 0.5
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(100.0))
+        ),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+                color: Colors.grey, width: 1.25
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(100.0))
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+                color: Colors.red, width: 1
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(100.0))
+        ),
+        contentPadding: EdgeInsets.only(left: 14.0, right: 6.0),
+        suffixIconColor: Colors.grey
+      ),
+    );
+  }
 }
+
