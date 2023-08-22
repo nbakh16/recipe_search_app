fe47c06a38329e3b9ecc479162ce92752d1e2858
Nabil160 <nbakhunjee97@gmail.com>
2023-08-22 20:13:42 +0600

    Initial

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
 
+import 'package:recipe_app/app/modules/details/bindings/details_binding.dart';
+import 'package:recipe_app/app/modules/details/views/details_view.dart';
 import 'package:recipe_app/app/modules/home/bindings/home_binding.dart';
 import 'package:recipe_app/app/modules/home/views/home_view.dart';
 
@@ -15,6 +17,11 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.DETAILS,
+      page: () => DetailsView(),
+      binding: DetailsBinding(),
     ),
   ];
 }
